e664b96b0c0d2d677beb08289f611913469f9d08
loipiytbjjj <mikelazard915@gmail.com>
2025-05-27 18:15:50 +0200

    Add files via upload

---
+++ kalash.js
@@ -0,0 +1,15 @@
+module.exports = {
+  token: process.env.token,
+  port: "3000",
+  prefix: "+",
+  client: "",
+  client_id: "1136494055405146193",
+  client_secret: "j1c1samYOj_HBrvC-DF7N8g-4-Ck_3bi",
+  redirect_uri: "https://auth-bot-credit-bossteam.ankityadav189.repl.co",
+  footer: "Axey Auth V999",
+  support: "",
+  wehbook: "", //wehbook de logs
+  owners: ["1137335547203952721", "1091698984575324322"],
+  authLink: `https://restorecord.com/verify/Wumpus%20Giveaways`,
+
+}
